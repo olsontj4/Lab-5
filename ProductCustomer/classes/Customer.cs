0cc7659f02e3afe4c861a1b9fc2f9909a3b309d2
olsontj4 <olsontj4@my.lanecc.edu>
2023-05-23 21:33:19 -0700

    CustomerList

---
+++ ProductCustomer/classes/Customer.cs
@@ -0,0 +1,83 @@
+﻿using System;
+
+namespace ProductCustomer.classes
+{
+    public class Customer
+    {
+        private string firstName;
+        private string lastName;
+        private string phone;
+        private string email;
+        private int id;
+
+        public Customer() { }
+
+        public Customer(string customerFirstName, string customerLastName, string customerPhone, string customerEmail, int customerId)
+        {
+            this.firstName = customerFirstName;
+            this.lastName = customerLastName;
+            this.phone = customerPhone;
+            this.email = customerEmail;
+            this.id = customerId;
+        }
+        public string FirstName
+        {
+            get
+            {
+                return firstName;
+            }
+            set
+            {
+                firstName = value;
+            }
+        }
+        public string LastName
+        {
+            get
+            {
+                return lastName;
+            }
+            set
+            {
+                lastName = value;
+            }
+        }
+        public string Phone
+        {
+            get
+            {
+                return phone;
+            }
+            set
+            {
+                phone = value;
+            }
+        }
+        public string Email
+        {
+            get
+            {
+                return email;
+            }
+            set
+            {
+                email = value;
+            }
+        }
+        public int Id
+        {
+            get
+            {
+                return id;
+            }
+            set
+            {
+                id = value;
+            }
+        }
+        public override string ToString()
+        {
+            return String.Format("First Name: {0} Last Name: {1} Phone Number: {2} Email: {3} ID: {4}", firstName, lastName, phone, email, id);
+        }
+    }
+}
---
+++ ProductCustomer/classes/CustomerList.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProductCustomer.classes
+{
+    public class CustomerList
+    {
+        private List<Customer> customers;
+
+        public CustomerList()
+        {
+            customers = new List<Customer>();
+        }
+
+        public int Count
+        {
+            get
+            {
+                return customers.Count;
+            }
+        }
+        public void Fill()
+        {
+            customers = CustomerDB.GetCustomers();
+        }
+        public void Save()
+        {
+            CustomerDB.SaveCustomers(customers);
+        }
+        public Customer this[int index]
+        {
+            get
+            {
+                return customers[index];
+            }
+
+        }
+        public Customer this[string email]
+        {
+            get
+            {
+                foreach (Customer customer in customers)
+                {
+                    if (customer.Email == email)
+                    {
+                        return customer;
+                    }
+                }
+                return null;
+            }
+        }
+        public void ChangeCustomer()
+        {
+
+        }
+        public void Add(Customer customer)
+        {
+            customers.Add(customer);
+        }
+        public void Add(string customerFirstName, string customerLastName, string customerPhone, string customerEmail, int customerId)
+        {
+            Customer c = new Customer(customerFirstName, customerLastName, customerPhone, customerEmail, customerId);
+            customers.Add(c);
+        }
+        public static CustomerList operator +(CustomerList cl, Customer c)
+        {
+            cl.Add(c);
+            return cl;
+        }
+        public void Remove(Customer customer)
+        {
+            customers.Remove(customer);
+        }
+        public static CustomerList operator -(CustomerList cl, Customer c)
+        {
+            cl.Remove(c);
+            return cl;
+        }
+        public override string ToString()
+        {
+            string output = "";
+            foreach (Customer c in customers)
+            {
+                output += c.ToString() + "\n";
+            }
+            return output;
+        }
+        /*
+        change the customer object in a specific position in the list*/
+    }
+}
